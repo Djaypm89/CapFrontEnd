1be6f855a6d392bc2ac5c4f88cf7c5358a1bb0c9
Djaypm89 <djaypm89@gmail.com>
2021-10-17 16:01:02 -0400

    home page update with buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,11 +6,38 @@ class App extends Component {
     constructor (props){
         super(props);
         this.stats = [
-            {location: "pass r2v location into here", statistics:"pass r2v info here"}
+            {location: "pass r2v location into here", statistics:"pass r2v info here"},
+            {location: "Miami, FL (hardcoded)", statistics:"rent: x, value: y. etc"},
+            {location: "Los Angeles, CA (hardcoded)", statistics:"rent: x, value: y. etc"}
+            
         ];
+
         this.state = {
             statNumber: 0
         };
+    }
+
+    goToNextLocaltion = () => {
+        // debugger;
+        let tempStatNumber = this.state.statNumber;
+        tempStatNumber++;
+        if(tempStatNumber === this.stats.length) {
+            tempStatNumber = 0;
+        }
+        this.setState({
+            statNumber: tempStatNumber
+        });
+    }
+
+    goToPreviousLocation = () => {
+        let tempStatNumber = this.state.statNumber;
+        tempStatNumber--;
+        if(tempStatNumber < 0) {
+            tempStatNumber = this.stats.length - 1;
+        }
+        this.setState({
+            statNumber: tempStatNumber
+        })
     }
 
     render(){
@@ -20,15 +47,17 @@ class App extends Component {
                 <div className="row">
                     <div className="col-md-4">
                         {/* more stuff here */}
+                        <button onClick={this.goToPreviousLocation}>Previous Location</button>
                     </div>
-                    
+
                     <div className="col-md-4">
                         <h1>{this.stats[this.state.statNumber].location}</h1>
-                        <h4>{this.stats[0].statistics}</h4>                        
+                        <h4>{this.stats[this.state.statNumber].statistics}</h4>
                     </div>
 
                     <div className="col-md-4">
                         {/* more stuff here */}
+                        <button onClick={this.goToNextLocaltion}>Next Location</button>
                     </div>
                 </div>
 
