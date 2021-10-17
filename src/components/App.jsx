74fe3b50ce9ce582fb02b3b9862e814f47d3059b
Djaypm89 <djaypm89@gmail.com>
2021-10-17 14:53:01 -0400

    title bar to display r2v data but still need to do from api call

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,33 @@ import TitleBar from './TitleBar/TitleBar';
 class App extends Component {
     constructor (props){
         super(props);
-        this.state = {};
+        this.stats = [
+            {location: "pass r2v location into here", statistics:"pass r2v info here"}
+        ];
+        this.state = {
+            statNumber: 0
+        };
     }
 
     render(){
         return (
             <div className="container-fluid">
                 <TitleBar />
+                <div className="row">
+                    <div className="col-md-4">
+                        {/* more stuff here */}
+                    </div>
+                    
+                    <div className="col-md-4">
+                        <h1>{this.stats[this.state.statNumber].location}</h1>
+                        <h4>{this.stats[0].statistics}</h4>                        
+                    </div>
+
+                    <div className="col-md-4">
+                        {/* more stuff here */}
+                    </div>
+                </div>
+
             </div>
         )
     }
