4d9abeea78720f3a886535aa1da798aec715b4d9
Djaypm89 <djaypm89@gmail.com>
2021-10-18 04:58:42 -0400

    created Statlist, displays on homepage.  added filters and inputs for search feature to map over StatList data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css'
+import StatList from './StatList/StatList';
 import TitleBar from './TitleBar/TitleBar';
 import StatViewer from './StatViewer/StatViewer';
 import StatCreator from './StatCreator/StatCreator'; 
@@ -13,12 +14,35 @@ class App extends Component {
             {location: "mock R2V location", data:"mock R2V data"},
             {location: "Miami, FL (hardcoded)", data:"rent: x, value: y. etc"},
             {location: "Los Angeles, CA (hardcoded)", data:"rent: x, value: y. etc"}
-            
         ];
 
         this.state = {
-            statNumber: 0
+            statNumber: 0,
+            filter : {city: '', state: '', country: ''},
+            city: '', country: '', state: ''
         };
+    }
+
+    clear(e) {
+        e.preventDefault()
+        this.setState({filter : {city: '', state: '', country: ''},
+                         city: '', country: '', state: ''})
+    }
+    filter(e) {
+        e.preventDefault();
+        this.setState({filter: {city: this.state.city, state: this.state.state, country: this.state.country}})
+    }
+
+    cityChanged(e) {
+        this.setState({city : e.target.value})
+    }
+
+    stateChanged(e) {
+        this.setState({state : e.target.value})
+    }
+
+    countryChanged(e) {
+        this.setState({country : e.target.value})
     }
 
     goToNextStat = () => {
@@ -55,13 +79,19 @@ class App extends Component {
     render(){
         return (
             <div className="container-fluid">
-                <TitleBar />
-                <StatViewer stat={this.stats[this.state.statNumber]} nextStat={this.goToNextStat} previousStat={this.goToPreviousStat}/>
-                <StatCreator createNewStat={this.createStat} />
-                <Footer />
+               
+                <StatList filter={this.state.filter} />
+              
+                <input type="text" value={this.state.city} onChange={e =>this.cityChanged(e) } placeHolder="City" />
+                <input type="text" value={this.state.state} onChange={e =>this.stateChanged(e) } placeHolder="State"   />
+                <input type="text" value={this.state.country} onChange={e =>this.countryChanged(e) } placeHolder="Country" />
+                <button onClick={e => this.filter(e) }>Filter</button>
+                <button onClick={e => this.clear(e) }>Clear</button>
             </div>
         )
     }
  }
 
  export default App;
+
+ 
---
+++ src/components/StatList/StatList.jsx
@@ -0,0 +1,109 @@
+import { stat } from 'fs';
+import React, {Component} from 'react';
+
+import './StatList.css'
+//import CreateStat from '../CreateStat/CreateStat';
+
+class StatList extends Component {
+    constructor(props) {
+       
+        super(props);
+        this.state = { 
+            stats: [],
+            search: ''
+         }
+    }
+
+    
+    componentDidMount(){
+        this.getStats()
+    }
+    
+    getStats(){
+       fetch('http://127.0.0.1:8000/housingdata/', {mode: 'cors'}
+       ).then(resp => resp.json()
+       ).then(json =>  this.setState({ stats: json }) )
+    }
+
+    handleChange = (event) =>{
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    
+    handleSubmit = (event) =>{
+        event.preventDefault();
+      
+    }
+   
+    buildTable() {
+        const result = []
+        const filter = this.props.filter;
+        console.log(filter)
+
+        for(let i = 0 ; i < this.state.stats.length ; i++) {
+            const stat = this.state.stats[i];
+            const record = (<tr key={stat.id}><td>{ stat.sizeRank }</td>
+                                        <td>{ stat.country }</td>
+                                        <td>{ stat.state }</td>
+                                        <td>{ stat.city}</td>
+                                        <td>{ stat.rent1YearAvg}</td>
+                                        <td>{ stat.rent5YearAvg}</td>
+                                        <td>{ stat.value1YearAvg}</td>
+                                        <td>{ stat.value5YearAvg}</td>
+
+                                        <td>{ stat.value1YearAvg ? (100 * stat.rent1YearAvg / stat.value1YearAvg).toFixed(1) : "" }</td>
+                                        <td>{ stat.value5YearAvg ? (100 * stat.rent5YearAvg / stat.value5YearAvg).toFixed(1) : "" }</td>
+
+                                    </tr>)
+            if(this.props.filter.city === '' || this.props.filter.city === stat.city) {
+                if(this.props.filter.state === '' || this.props.filter.state === stat.state) {
+                    if(this.props.filter.country === '' || this.props.filter.country === stat.country) {
+                          result.push(record)
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    render() { 
+        return (
+        <React.Fragment>
+            <table className="table styled-table">
+                <thead>
+                    <tr>
+                    <th>Rank</th>
+                    <th>Country</th>
+                    <th>State</th>
+                    <th>City</th>
+                    <th>Rent 1 Year Avg</th>
+                    <th>Rent 5 year Avg</th>
+                    <th>Value 1 Year Avg</th>
+                    <th>Value 5 year Avg</th>
+                    <th>Ratio 1 year</th>
+                    <th>Ratio 5 year</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                       this.buildTable()
+                    }
+                </tbody>
+            </table>
+      
+            <table class='styled-table'>
+                <tbody>
+                <tr>
+                    <td class="active-row">
+                    
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            
+        </React.Fragment>  
+         );
+    }
+}
+ 
+export default StatList;
